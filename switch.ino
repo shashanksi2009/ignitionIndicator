17b3f081e36909f59fb2438d0dcbcd5e1610bc95
Shashank Singh <shashanksi2009@gmail.com>
2016-05-10 04:01:23 -0700

    Add files via upload

---
+++ switch.ino
@@ -0,0 +1,11 @@
+void setup() {
+  // put your setup code here, to run once:
+ pinMode(3,INPUT);
+ pinMode(4,OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+digitalWrite(4,digitalRead(3));
+delay(20);
+}
